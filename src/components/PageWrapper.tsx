79a6897756dd207ffda8e63720b97c2e71f3f4d5
addoxy <adityakumar13043@gmail.com>
2024-03-15 22:06:27 +0530

    chore:  setup

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,22 +1,21 @@
-"use client";
+'use client';
 
-import { Toaster } from "./vendor/sonner";
-import Providers from "@/utils/providers";
-import { cn } from "@/utils/utils";
-import { ReactNode } from "react";
+import { Toaster } from './vendor/sonner';
+import { cn } from '@/utils/utils';
+import { ReactNode } from 'react';
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <Providers>
+    <>
       <Toaster position="top-right" />
       <main
         className={cn(
-          "mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-4 py-10 sm:px-10 lg:px-16 xl:px-24"
+          'mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-4 py-10 sm:px-10 lg:px-16 xl:px-24'
         )}
       >
         {children}
       </main>
-    </Providers>
+    </>
   );
 };
 
