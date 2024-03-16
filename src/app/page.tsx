4c902f051ad418963940a8ded7ebee585536f484
addoxy <adityakumar13043@gmail.com>
2024-03-16 15:47:05 +0530

    chore:  add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <>
-      <main></main>
-    </>
+    <main className="flex flex-col">
+      <h1 className="mb-5 text-5xl font-semibold text-zinc-50">
+        Hi. I&apos;m Addoxy.
+      </h1>
+      <p className="mb-5 text-2xl font-medium text-zinc-300">
+        Front-end developer
+      </p>
+      <p className="mb-7 max-w-2xl text-xl leading-8 text-zinc-400">
+        I build <span className="text-sky-500">beautiful, interactive</span> and{' '}
+        <span className="text-sky-500">accessible</span> websites with a focus
+        on seamless user experiences
+      </p>
+    </main>
   );
 }
--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -7,7 +7,7 @@ const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <main
       className={cn(
-        'mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-4 py-10 sm:px-10 lg:px-16 xl:px-24'
+        'mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-6 py-48 sm:px-32 lg:px-64 xl:px-72'
       )}
     >
       {children}
