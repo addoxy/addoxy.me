ccfc56044ec53834b4b9080e2a65199af21fa5ef
addoxy <adityakumar13043@gmail.com>
2024-03-16 15:29:43 +0530

    chore:  remove toaster component

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,21 +1,17 @@
 'use client';
 
-import { Toaster } from './vendor/sonner';
 import { cn } from '@/utils/utils';
 import { ReactNode } from 'react';
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <Toaster position="top-right" />
-      <main
-        className={cn(
-          'mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-4 py-10 sm:px-10 lg:px-16 xl:px-24'
-        )}
-      >
-        {children}
-      </main>
-    </>
+    <main
+      className={cn(
+        'mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-4 py-10 sm:px-10 lg:px-16 xl:px-24'
+      )}
+    >
+      {children}
+    </main>
   );
 };
 
